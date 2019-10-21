58d1cc8d3256326f2fee9969be09da89c743fd13
detlef <ds2@physik.de>
2019-10-21 17:32:35 +0200

    - memory optimization, first run possbile

--- LearnPython.py
+++ LearnPython.py
@@ -11,6 +11,7 @@ from tensorflow.keras import Model
 from tensorflow.keras.layers import Activation, Embedding, Dense,  Lambda, Input
 from tensorflow.keras.layers import SimpleRNN, GRU, LSTM
 from tensorflow.keras.utils import to_categorical
+import numpy as np
 
 parser = argparse.ArgumentParser(description='train recurrent net.')
 parser.add_argument('--lr', dest='lr',  type=float, default=1e-3)
@@ -24,13 +25,14 @@ parser.add_argument('--debug', dest='debug', action='store_true')
 parser.add_argument('--only_one', dest='only_one', action='store_true')
 parser.add_argument('--revert', dest='revert', action='store_true')
 parser.add_argument('--add_history', dest='add_history', action='store_true')
-parser.add_argument('--RNN_type', dest='RNN_type',  type=str, default='LSTM')
+parser.add_argument('--RNN_type', dest='RNN_type',  type=str, default='GRU')
 parser.add_argument('--gpu_mem', dest='gpu_mem',  type=float, default=1)
 parser.add_argument('--fill_vars_with_atoms', dest='fill_vars_with_atoms', action='store_true')
 parser.add_argument('--rand_atoms', dest='rand_atoms', action='store_true')
 parser.add_argument('--float_type', dest='float_type',  type=str, default='float32')
 
 parser.add_argument('--limit_files', dest='limit_files',  type=int, default=0)
+parser.add_argument('--epoch_size', dest='epoch_size',  type=int, default=1000)
 
 args = parser.parse_args()
 
@@ -51,17 +53,18 @@ used_ords = {}
 used_ords['\n']=0
 num_ord = [1] # starting the translation table 0 reserved to seperate lines which are striped here
 
+
 def load_dataset(file_names):
     global num_ord
     data_set = []
     count_files = 0
-    for line in file_names:
+    for filename in file_names:
         if args.limit_files > 0:
             if count_files > args.limit_files:
                 break
             count_files +=1
         try:
-            py_program = read_file(line.strip())
+            py_program = read_file(filename.strip())
             program_lines = []
             for t2 in py_program:
                 program_chars = []
@@ -71,9 +74,9 @@ def load_dataset(file_names):
                         num_ord[0] += 1
                     program_chars.append(used_ords[ord(t3)])
                 program_lines.append(program_chars)
-            data_set.append(program_lines)
+            data_set.append(filename)
         except UnicodeDecodeError as e:
-            print(line + '\n  wrong encoding ' + str(e))
+            print(filename + '\n  wrong encoding ' + str(e))
     return data_set
 
 train_data_set = load_dataset(read_file('python100k_train.txt'))    
@@ -102,17 +105,29 @@ class KerasBatchGenerator(object):
     def generate(self):
         while True:
             for python_file in self.data_set:
+                py_program = read_file(python_file.strip())
+                program_lines = []
+                for t2 in py_program:
+                    program_chars = []
+                    for t3 in t2:
+                        if ord(t3) not in used_ords:
+                            used_ords[ord(t3)] = num_ord[0]
+                            num_ord[0] += 1
+                        program_chars.append(used_ords[ord(t3)])
+                    program_lines.append(program_chars)
                 full_python_file_string = []
-                for python_line in python_file:
+                for python_line in program_lines:
                     full_python_file_string.extend(python_line)
                     full_python_file_string.append(0)
-                    
-                yield full_python_file_string, to_categorical(full_python_file_string, num_classes=max_output)
+                tmp_x =  np.array([full_python_file_string], dtype=int)
+                tmp_y = np.array([full_python_file_string], dtype=int)
+                yield tmp_x, to_categorical(tmp_y, num_classes=max_output)
 
 
-train_data_generate = KerasBatchGenerator(test_data_set).generate()
+train_data_generator = KerasBatchGenerator(train_data_set)
+test_data_generator = KerasBatchGenerator(test_data_set)
 count=0
-for tt in train_data_generate:
+for tt in train_data_generator.generate():
     print(tt)
     count+=1
     if count>10:
@@ -152,10 +167,21 @@ else:
     lstm1b = Lambda(attentions_layer)(lstm1)
   else:
     lstm1b = lstm1
-  lstm4 = LSTM_use(hidden_size, return_sequences=False)(lstm1b)
+  lstm4 = LSTM_use(hidden_size, return_sequences=True)(lstm1b)
 
-  x = Dense(max_output +1)(lstm4)
+  x = Dense(max_output)(lstm4)
   predictions = Activation('softmax')(x)
   model = Model(inputs=inputs, outputs=predictions)
+
+print("starting",args)
+#checkpointer = ModelCheckpoint(filepath='checkpoints/model-{epoch:02d}.hdf5', verbose=1)
+
+num_epochs = args.epochs
+
+model.compile(loss='categorical_crossentropy', optimizer = 'SGD', metrics=['categorical_accuracy'])
+history = model.fit_generator(train_data_generator.generate(), args.epoch_size, num_epochs, validation_data=test_data_generator.generate(), validation_steps=args.epoch_size / 10) 
+
+
+
 
 
