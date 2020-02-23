3a5f5f37a732d1e336504f14953bef6f307a19eb
detlef <ds2@physik.de>
2020-02-23 19:19:35 +0100

    - start a toy playground for test with programming by specification

---
+++ ToyProgBySpec.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 18:53:25 2020
+
+@author: detlef
+"""
+
+from tkinter import Tk, Canvas, mainloop, W
+master = Tk()
+
+canvas_width = 800
+canvas_height = 400
+w = Canvas(master, 
+           width=canvas_width,
+           height=canvas_height)
+w.pack()
+
+def click(event):
+    print('button clicked',event)
+
+
+def key(event):
+    w.create_text(20, 30, anchor=W, font="Purisa",
+            text=event.char)
+    print('key pressed',event)
+    
+    
+w.bind('<Button-1>', click)
+master.bind('<Key>',key)
+mainloop()
