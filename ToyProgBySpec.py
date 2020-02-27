af37435cbd867aeca26f7a175416d7e256fed6d1
detlef <ds2@physik.de>
2020-02-27 08:29:20 +0100

    - prepared

--- ToyProgBySpec.py
+++ ToyProgBySpec.py
@@ -15,6 +15,14 @@ def getVariable(name):
 
 # takes a lambda function at the moment to compare two elements
     # e.g. between(ll,lambda a:ll[a].getVar('left')-ll[a-1].getVar('right'))
+def for_all(ll, compare):
+    ret=[]
+    if len(ll)>1:
+        for i in range(1,len(ll)):
+            print(i,compare(i))
+            ret.append(compare(i))
+    return ret
+
 def between(ll, compare):
     ret=[]
     if len(ll)>1:
@@ -34,7 +42,8 @@ class BaseRules():
             
         self.TheChanges = {} #contains the variables to be changed in priority during a reassignement
         self.childs = []
-        
+        self.not_optimizing = None
+            
     def getVar(self, name):
         return self.TheVars[name]
     
@@ -45,6 +54,11 @@ class BaseRules():
         raise ValueError('Can not take Elements from before.')
         
     def optimize(self):
+        
+        #handle childs!!!!
+        
+        
+        
         opt_vars = self.priority
         opt_vars.reverse()
         for vv in opt_vars:
@@ -70,8 +84,9 @@ class BaseRules():
 # This is the main documented class, later classes are not documented for future syntax
 class Character(BaseRules):
     def __init__(self, ch):
-        self.priority = ['top', 'left', 'height', 'width', 'right', 'bottom'] #Later this should be syntactically improved
-    
+        self.priority = ['top', 'left', 'right', 'bottom'] #Later this should be syntactically improved
+        self.not_optimizing = ['height', 'width']
+        
         #fixed content not changed by other variables
         self.TheCharacter = ch
         BaseRules.__init__(self)
@@ -103,6 +118,8 @@ class Word(BaseRules):
         if (self.WordCharacters.len>0):
             ret.append(self.WordCharacters[0].getVar('left')-self.getVar('left'))
             ll=self.WordCharacters
+            ret += for_all(ll, lambda a: ll[a].getVar('top')-self.getVar('top'))
+            ret += for_all(ll, lambda a: ll[a].getVar('bottom')-self.getVar('bottom'))
             ret += between(ll, lambda a: ll[a].getVar('left')-ll[a-1].getVar('right'))
             ret.append(self.WordCharacters[len(self.WordCharacters)-1].getVar('right')-self.getVar('right'))
         return ret, None
@@ -171,30 +188,7 @@ class Page(BaseRules):
 
 
 
-# define operations between objects ?????????
 # takeToMuch can take from before, restrictions can return ToLong
-
-
-
-
-
-
-
-# train lambda
-a=Character('a')
-a.setVar('left',3)
-a.setVar('right',5)
-b=Character('b')
-b.setVar('left',6)
-b.setVar('right',9)
-c=Character('c')
-c.setVar('left',15)
-c.setVar('right',20)
-ll=[a,b,c]
-between(ll,lambda a:ll[a].getVar('left')-ll[a-1].getVar('right'))
-
-
-#a.takeToMuch(None)
 
 testpage = Page()
 testpage.setVar('left',0)
@@ -204,17 +198,6 @@ testpage.setVar('right',0)
 
 actualLine = testpage.addLine()
 actualWord = actualLine.addWord()
-
-
-
-
-
-
-
-
-
-
-
 
 # Toy Manager
 master = Tk()
